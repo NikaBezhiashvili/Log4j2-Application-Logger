3f3f275b55cbd72b0270645141845a0b02e7e4a1
NikaBezhiashvili <74880697+NikaBezhiashvili@users.noreply.github.com>
2023-05-10 20:56:48 +0400

    Update HelloSpark.py

--- HelloSpark.py
+++ HelloSpark.py
@@ -3,6 +3,7 @@ import os
 import sys
 from logger import Log4J
 from utils import *
+from pyspark.sql.functions import  desc
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -19,7 +20,11 @@ logger = Log4J(spark)
 
 logger.info('Starting HelloSpark')
 
-read_dataframe(spark, "datafiles/dataset.csv").show()
+rawData = read_dataframe(spark, "datafiles/dataset.csv")
+filtered_data = rawData.where(rawData['Business Account Number'] < 750) \
+                        .select('Business Account Number', 'City', 'Mail Zipcode') \
+                        .orderBy(desc('Business Account Number'))
+filtered_data.show()
 
 logger.info('Finished HelloSpark')
 
