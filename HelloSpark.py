9e1b0c9ae4903c2a44819206936e52f9218aacf4
NikaBezhiashvili <74880697+NikaBezhiashvili@users.noreply.github.com>
2023-05-10 20:09:50 +0400

    Update HelloSpark.py

--- HelloSpark.py
+++ HelloSpark.py
@@ -2,9 +2,12 @@ from pyspark.sql import *
 import os
 import sys
 from logger import Log4J
+from utils import *
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+os.environ['LOG4J_CONFIGURATION'] = 'file:C:/Users/Administrator/Desktop/Data/HelloSpark/log4j2.properties'
+
 
 
 spark = SparkSession.builder \
@@ -15,6 +18,8 @@ spark = SparkSession.builder \
 logger = Log4J(spark)
 
 logger.info('Starting HelloSpark')
+
+read_dataframe(spark, "datafiles/dataset.csv").show()
 
 logger.info('Finished HelloSpark')
 
